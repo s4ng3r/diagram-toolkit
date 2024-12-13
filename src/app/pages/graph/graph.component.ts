ca4c30cc5f06f77d57eeb9533c561fc83ad7f90d
s4ng3r <s4ng3r@gmail.com>
2024-12-13 09:45:29 -0300

    Added mermeid lib as local asset with types Added server routes for hybrid  rendering

--- src/app/pages/graph/graph.component.ts
+++ src/app/pages/graph/graph.component.ts
@@ -1,20 +1,69 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  Inject,
+  Input,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
+import { Mermaid } from 'mermaid';
+
+declare const mermaid: Mermaid;
 
 @Component({
   selector: 'app-graph',
-  imports: [],
+  standalone: true,
   templateUrl: './graph.component.html',
-  styleUrl: './graph.component.css',
+  styleUrls: ['./graph.component.css'],
 })
-export class GraphComponent implements OnInit {
+export class GraphComponent implements OnInit, AfterViewInit {
   @Input() team!: string;
   @Input() graph!: string;
 
-  constructor(private route: ActivatedRoute) {}
+  mmd!: Mermaid;
+  mermaidGraph = `
+    graph TD
+      A[Start] --> B{Is it sunny?}
+      B -- Yes --> C[Go outside]
+      B -- No --> D[Stay inside]
+  `;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: never) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.mmd = mermaid;
+    }
+  }
 
   ngOnInit(): void {
     console.log('team: ', this.team);
-    console.log('diagram: ', this.graph);
+    console.log('graph: ', this.graph);
+
+    if (isPlatformBrowser(this.platformId)) {
+      this.mmd.initialize({
+        startOnLoad: true,
+        securityLevel: 'loose',
+      });
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        this.renderMermaid();
+      }, 0);
+    }
+  }
+
+  async renderMermaid(): Promise<void> {
+    const graphElement = document.querySelector('#mermaid-graph');
+
+    if (graphElement) {
+      const { svg } = await this.mmd.render(
+        'mermaid-graph2',
+        this.mermaidGraph
+      );
+      graphElement.innerHTML = svg;
+    }
   }
 }
